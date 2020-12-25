6cd77cb5c7de3869b7225089717c8982ae542f23
torchfish <814671035@qq.com>
2020-12-25 13:10:54 +0800

    SJAIDFJS

---
+++ 201811050989/grade.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+
+{
+	int sum_student;
+    scanf("%d",&sum_student);
+	struct student
+	{
+		int no;	
+		char name[5];	
+		int grade_chinese;	
+		int grade_math;	
+		int grade_english;
+		int sum_grade;
+	}std,pers[8];
+
+for(i=0;i<sum_student;i++)
+{
+    pers[i].no=i+1;
+    scanf("%c %d %d %d",&pers[i].name[5],&pers[i].grade_chinese,&pers[i].grade_math,&pers[i].grade_english);
+	pers[i].sum_grade=pers[i].grade_chinese+pers[i].grade_math+pers[i].grade_english;
+}
+
+	if(pers[i].sum_grade>pers[i].sum_grade)
+
+	{
+if(.sum_grade=.sum_grade)
+{
+	if(.grade_chinese>.grade_chinese)
+if(.no=.no)
+
+
+for(m=0;m<5;m++)
+{
+	printf("%d %d",&pers[m].no,&pers[m].sum_grade)
